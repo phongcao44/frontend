5134f81c23dfe2bb8342d8686071eea9158bdf80
phong <phonglx001@gmail.com>
2025-08-15 16:45:17 +0700

    a

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
